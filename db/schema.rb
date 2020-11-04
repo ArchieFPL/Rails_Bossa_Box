09f821fa915398de27be53719aa51a1d3d91ff1e
ArchieFPL <franciscoparadella@gmail.com>
2020-11-03 21:00:17 -0300

    Rubocop fixes

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,20 +12,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_03_204556) do
-
+ActiveRecord::Schema.define(version: 20_201_103_204_556) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "tools", force: :cascade do |t|
-    t.string "title"
-    t.string "link"
-    t.string "description"
-    t.string "tags", array: true
-    t.string "true"
-    t.string "created_by"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'tools', force: :cascade do |t|
+    t.string 'title'
+    t.string 'link'
+    t.string 'description'
+    t.string 'tags', array: true
+    t.string 'true'
+    t.string 'created_by'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
-
 end
